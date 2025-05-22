384f6a0bbce8a3820024fe7fd733cff8da26fb41
AnthonyFerrerG <anthonydario999710@gmail.com>
2025-05-21 22:06:15 -0300

    AffirmationList.kt

---
+++ ui/AffirmationList.kt
@@ -0,0 +1,19 @@
+package com.example.affirmations.ui
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.affirmations.model.Affirmation
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(
+                affirmation = affirmation,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
